dfbaf0c30f6902d372c59841e2f38d0beebbd8e5
Carl Ulsøe Christensen <34673973+carlulsoe@users.noreply.github.com>
2019-10-24 14:36:19 +0200

    updated solver

--- Queens/Solver.java
+++ Queens/Solver.java
@@ -1,27 +1,74 @@
-
+import java.util.*;
 public class Solver
 {
     private int noOfQueens;
     private int[] queens;
     private int noOfSolutions;
-    
+
     public void findAllSolutions(int noOfQueens) {
+        queens = new int[noOfQueens];
+        Arrays.fill(queens, -1);
+        positionQueens(0);
+
+    }
+
+    private void positionQueens(int row)  {
+        if (row != noOfQueens) {
+            for (int col = 0; col <= noOfQueens; col++) {
+                if (legal(row, col)) {
+                    queens[row] = col; 
+                    positionQueens(row+1);
+                }
+            }
+        }
+    }
+
+    private boolean legal(int row, int col) {        
         
+        for (int i = 1; i < col; i++) {
+            if (queens[col - i] == row - i) {
+                return false;
+            }
+        }
+        for (int i = 1; i < noOfQueens - col; i++) {
+            if (queens[col + i] == row - i) {
+                return false;
+            }
+        }
+        /*
+        if (queens[col] != 0) {
+            return false;
+        }*/
+        return true;
     }
-    
-    private void positionQueens(int row) {
-        
-    }
-    
-    private boolean legal(int row, int col) {
-    
-    }
-    
+
     private void printSolution() {
-        
+        System.out.println("*****************************");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
     }
-    
+
     private String convert(int row, int col) {
-        
+        col += 1;
+        char r = (char) (97 + row);
+        return r + Integer.toString(col);
     }
+
+    public void testLegal(int n, int... pos) {
+        noOfQueens = n;
+        queens = Arrays.copyOf(pos, n);
+
+        System.out.print(n + "x" + n + " with queens in: " +
+            Arrays.toString(pos) + " => Legal positions: ");
+
+        for (int i = 0; i < n; i++) {
+            if (legal(pos.length, i)) { 
+                System.out.print(i + " "); 
+            }
+        }
+        System.out.println();
+    }
+    //testLegal(8, 4, 6, 3, 5);
 }
+
