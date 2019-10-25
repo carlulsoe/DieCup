48971a86fa100f3b5628938bab05ddb5d84cba0f
Carl Ulsøe Christensen <34673973+carlulsoe@users.noreply.github.com>
2019-10-25 09:28:13 +0200

    Switched to Intellij

--- Queens/src/Main.java
+++ Queens/src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        Solver solver = new Solver();
-        solver.testLegal(8, 4, 6, 3, 5);
+        TestSolver.legalTest();
     }
 }
