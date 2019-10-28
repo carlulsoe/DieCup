243b6b483f889d08d289a0ddf37b14e6c9506fdd
Carl Ulsøe Christensen <34673973+carlulsoe@users.noreply.github.com>
2019-10-28 12:51:46 +0100

    Added maxQueens

--- Queens/src/Main.java
+++ Queens/src/Main.java
@@ -3,5 +3,6 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         TestSolver.legalTest();
+        //TestSolver.maxQueens();
     }
 }
--- Queens/src/TestSolver.java
+++ Queens/src/TestSolver.java
@@ -12,7 +12,9 @@ public class TestSolver
         s1.findAllSolutions(1);
         s1.findAllSolutions(2);
         s1.findAllSolutions(6);
-        s1.findNoOfSolutions(1, 12);
+        System.out.println("");
+        s1.findNoOfSolutions(5, 12);
+        System.out.println("");
         s1.testLegal(8, 4, 6, 3, 5);
         s1.testLegal(8, 0, 2, 4);
         s1.testLegal(8, 0, 2, 4, 6);
@@ -20,4 +22,21 @@ public class TestSolver
         s1.testLegal(8, 0, 3);
 
     }
+
+    public static void maxQueens() {
+        Solver solver = new Solver();
+        solver.setShowSolutions(false);
+        long start = 0;
+        long end = 0;
+        int i = 1;
+        while (end - start < 3*60*1000) {
+            start = System.currentTimeMillis();
+            solver.findAllSolutions(i);
+            end = System.currentTimeMillis();
+            System.out.println(i);
+            i++;
+            }
+        System.out.println(i-1);
+    }
+
 }
